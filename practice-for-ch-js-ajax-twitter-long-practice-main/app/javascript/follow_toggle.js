f6286894cbf5b67be7730cf3f9fbe25aff533812
Fahim Khan <fk652@nyu.edu>
2023-03-08 14:17:49 -0500

    finish ajax follow/unfollow

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/follow_toggle.js
@@ -3,24 +3,72 @@ import { API, broadcast } from "./util";
 export default class FollowToggle {
   constructor(toggleButton) {
     // Your code here
+    this.toggleButton = toggleButton;
+    this.toggleButton.addEventListener('click', this.handleClick.bind(this));
   }
 
   async handleClick(event) {
     // Your code here
+    event.preventDefault();
+    // console.log(this.followState);
+    if (this.followState === "followed") {
+      this.unfollow();
+    } else if (this.followState === "unfollowed") {
+      this.follow();
+    }
   }
 
   async follow() {
     // Your code here
+    // console.log(this.toggleButton.dataset.dataUserId);
+    // console.log(this.toggleButton.getAttribute("data-user-id"));
+
+    this.followState = "following";
+    const data = await API.followUser(this.toggleButton.getAttribute("data-user-id"));
+    if (data) {
+      console.log("Success!");      
+      this.followState = "followed";
+    } else {
+      console.log("Failure!")
+    }
   }
 
   async unfollow() {
     // Your code here
+    // console.log(this.toggleButton.dataset.dataUserId);
+    // console.log(this.toggleButton.getAttribute("data-user-id"));
+
+    this.followState = "unfollowing";
+    const data = await API.unfollowUser(this.toggleButton.getAttribute("data-user-id"));
+    if (data) {
+      console.log("Success!");      
+      this.followState = "unfollowed";
+    } else {
+      console.log("Failure!")
+    }
   }
 
   render() {
     switch (this.followState) {
       // Your code here
+      case "followed":
+        this.toggleButton.disabled = false;
+        this.toggleButton.innerText = "Unfollow!";
+        break;
+      case "unfollowed":
+        this.toggleButton.disabled = false;
+        this.toggleButton.innerText = "Follow!";
+        break;
+      case "following":
+        this.toggleButton.disabled = true;
+        this.toggleButton.innerText = "Following...";
+        break;
+      case "unfollowing":
+        this.toggleButton.disabled = true;
+        this.toggleButton.innerText = "Unfollowing...";
+        break;
     }
+    // console.log(this.followState);
   }
 
   get followState() {
--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -3,8 +3,37 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
-    ...options.headers
+    ...options.headers,
+    'X-CSRF-Token': csrfToken,
+    "Content-Type": "application/json",
+    "Accept": "application/json"
   };
 
-  return await fetch(url, options);
+  const response = await fetch(url, options);
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error(response);
+    // throw response;
+  }
 }
+
+export function followUser(id) {
+  return customFetch(
+    `/users/${id}/follow`,
+    {
+      method: "POST"
+    }
+  )
+}
+
+export function unfollowUser(id) {
+  return customFetch(
+    `/users/${id}/follow`,
+    {
+      method: "DELETE"
+    }
+  )
+}
+
+// export const foo = "bar";
